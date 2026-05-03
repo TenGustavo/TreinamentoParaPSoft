012c0b1dd46643e62ee476eb88322f2da1085fcf
TenGustavo <gustavo.luiz.ferreira.de.souza@ccc.ufcg.edu.br>
2026-05-03 19:05:03 -0300

    Cliente

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+
+    private String email;
+    private String telefone;
+    private String nome;
+    private int    idade;
+    private double salario;
+
+    public Cliente(String email, String telefone, String nome, int idade, double salario){
+        this.email = email;
+        this.telefone = telefone;
+        this.nome = nome;
+        this.idade = idade;
+        this.salario = salario;
+    }
+}
