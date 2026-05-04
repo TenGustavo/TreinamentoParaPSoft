bed2bb835211562daf8f2b22881962ed4ad27e45
TenGustavo <gustavo.luiz.ferreira.de.souza@ccc.ufcg.edu.br>
2026-05-03 22:35:56 -0300

    Venda

---
+++ Venda.java
@@ -0,0 +1,12 @@
+public class Venda{
+    
+    private int codVenda;
+    private double valor;
+    private ArrayList<String> pecas;
+
+    public Venda(int cod, double valor){
+        this.codVenda = cod;
+        this. valor = valor;
+        this.pecas = new ArrayList<String>;
+    }
+}
