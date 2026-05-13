d8349bb5ade8cd56abfeedd3b7db337299f4821b
TenGustavo <gustavo.luiz.ferreira.de.souza@ccc.ufcg.edu.br>
2026-05-12 21:56:31 -0300

    DiaAntesDaProva.java e Mouse.java

---
+++ Mouse.java
@@ -0,0 +1,12 @@
+Public Class Mouse{
+
+    private int tamanho;
+    private String marca;
+    private boolean ligado;
+
+    public Mouse(int tamanho, String marca, boolean ligado){
+        this.tamanho = tamanho;
+        this.marca = marca;
+        this.ligado = false;
+    }
+}
